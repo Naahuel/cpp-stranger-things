0ee494c517c7562cb11668cd578c995f223e3cc5
Nahuel José <nahuel.jose@globant.com>
2019-09-18 17:57:19 -0300

    Attempt to cache images

--- src/Wall.js
+++ src/Wall.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const Wall = ({letterCode}) => {
-  let imageName = (parseInt(letterCode) > 64 && parseInt(letterCode) < 91) ? letterCode : 'vacio';
-  let image = require(`./assets/images/${imageName}.gif`);
+class Wall extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="Wall">
-      <img src={image} alt="Stranger Things" />
-    </div>
-  );
+    this.state = {
+      images: []
+    }
+  }
+
+  componentDidMount() {
+    // Pre-cachear imagenes
+    let images = {
+      vacio: require(`./assets/images/vacio.gif`)
+    };
+
+    for (let index = 65; index < 91; index++) {
+      // create an Image instance
+      let img = new Image()
+      // setting handler for load complete
+      // set the src attribute for it. After this line image loading will start
+      img.src = require(`./assets/images/${index}.gif`);
+      images[index] = img.src;
+    }
+
+    this.setState({
+      images
+    });
+  }
+
+  render() {
+    let imageName = (parseInt(this.props.letterCode) > 64 && parseInt(this.props.letterCode) < 91) ? this.props.letterCode : 'vacio';
+
+    return (
+      <div className="Wall">
+        <img src={this.state.images[imageName]} alt="Stranger Things" />
+      </div>
+    );
+  }
 }
 
 export default Wall;
